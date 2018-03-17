85b0d7210ce552c5791b1bb4711f1cb3e5e16de7
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2018-03-17 18:01:30 +0900

    def _log to echo message in same format

--- java/Test.java
+++ java/Test.java
@@ -1,14 +1,16 @@
 public class Test {
     public String name;
 
-    public Test() { this("nothing"); }
-    public Test(String _n) { this.name = _n; System.out.println(this.name + " standby"); }
+    public void _log(String s) { System.out.println("[" + this.name + "]: " + s); }
 
-    public void v() { System.out.println(this.name + ": void test"); return; }
-    public long l(long l) { System.out.println(this.name + ": long test"); return 2*l; }
-    public double d(double d) { System.out.println(this.name + ":double test"); return 10*d; }
+    public Test() { this("nothing"); }
+    public Test(String _n) { this.name = _n; this._log("standby"); }
+
+    public void v() { this._log("void test"); return; }
+    public long l(long l) { this._log("long test"); return 2*l; }
+    public double d(double d) { this._log("double test"); return 10*d; }
     //
-    public String s(String s) { System.out.println(this.name + ": string test"); return ("hello " + s); }
-    public long[] lv(long[] lv) {  System.out.println(this.name + ": lvector test"); for ( int i=0; i<lv.length; i++ ) lv[i] = 2*lv[i]; return lv; }
-    public double[] dv(double[] dv) {  System.out.println(this.name + ": dvector test"); for ( int i=0; i<dv.length; i++ ) dv[i] = 10*dv[i]; return dv; }
+    public String s(String s) { this._log("string test"); return ("hello " + s); }
+    public long[] lv(long[] lv) {  this._log("lvector test"); for ( int i=0; i<lv.length; i++ ) lv[i] = 2*lv[i]; return lv; }
+    public double[] dv(double[] dv) {  this._log("dvector test"); for ( int i=0; i<dv.length; i++ ) dv[i] = 10*dv[i]; return dv; }
 }
