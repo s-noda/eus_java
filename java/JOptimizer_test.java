b7c4334dba2fbe67b1d822c58c027523c1b4aeca
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2018-03-25 09:19:22 +0900

    add log4j.properties

--- java/JOptimizer_test.java
+++ java/JOptimizer_test.java
@@ -18,7 +18,12 @@ public class JOptimizer_test {
 
     public void initOptiizationRequest() { or = new OptimizationRequest(); }
 
+    public void echoOptimizationSettings() {
+	System.out.println("itter < " + this.or.getMaxIteration());
+	System.out.println("KKT   ? " + this.or.isCheckKKTSolutionAccuracy());
+    }
     public void setInitialPoint(double[] x) { this.or.setInitialPoint(x); }
+    public void disableKKT() { this.or.setCheckKKTSolutionAccuracy(false); this.or.setCheckProgressConditions(false); }
     public void setTolerance(double r) {
 	this.or.setToleranceKKT(r); this.or.setToleranceFeas(r); this.or.setTolerance(r); }
     public void setQuadraticObjective(double[] _P, double[] q, double r) {
@@ -68,8 +73,10 @@ public class JOptimizer_test {
 	JOptimizer_test jt = new JOptimizer_test();
 	jt.initOptiizationRequest();
 	jt.setTolerance(1e-3);
+	jt.disableKKT();
 	jt.setInitialPoint(new double[]{1,1});
-	jt.setQuadraticObjective(new double[]{1,0,0,1}, new double[]{0,0}, 0);
+	jt.setQuadraticObjective(new double[]{1,1,0,1}, new double[]{0,0}, 0);
+	jt.echoOptimizationSettings();
 	if ( jt.optimize() > 0) {
 	    double[] ret =jt.solution();
 	    for ( int i=0; i<ret.length; i++ )
@@ -77,35 +84,4 @@ public class JOptimizer_test {
 	} else {
 	    System.out.println("fail");
 	}}
-
-    // public static void main(String[] args) throws Exception {
-    //     // Objective function (plane)
-    //     LinearMultivariateRealFunction objectiveFunction = new LinearMultivariateRealFunction(new double[] {3.0, 4.0}, 0); //minimize 3x+4y
-    //     //inequalities (polyhedral feasible set G.X<H )
-    //     ConvexMultivariateRealFunction[] inequalities = new ConvexMultivariateRealFunction[4];
-    //     // x >= 0
-    //     inequalities[0] = new LinearMultivariateRealFunction(new double[]{-1.0, 0.00}, 0.0);  // focus: -x+0 <= 0 
-    //     // y >= 0
-    //     inequalities[1] = new LinearMultivariateRealFunction(new double[]{0.0, -1.00}, 0.0);  // focus: -y+0 <= 0
-    //     // 2x+3y >= 8
-    //     inequalities[2] = new LinearMultivariateRealFunction(new double[]{-2.0, -3.00}, 8.0); // focus: -2x-3y+8 <= 0
-    //     // 5x+2y >= 12
-    //     inequalities[3] = new LinearMultivariateRealFunction(new double[]{-5.0, -2.00}, 12.0);// focus: -5x-2y+12 <= 0
-    //     //optimization problem
-    //     OptimizationRequest or = new OptimizationRequest();
-    //     or.setF0(objectiveFunction);
-    //     or.setFi(inequalities);
-    //     //or.setInitialPoint(new double[] {0.0, 0.0});//initial feasible point, not mandatory
-    //     or.setToleranceFeas(1.E-9);
-    //     or.setTolerance(1.E-9);
-    //     //optimization
-    //     JOptimizer opt = new JOptimizer();
-    //     opt.setOptimizationRequest(or);
-    // 	opt.optimize();
-    //     double[] sol = opt.getOptimizationResponse().getSolution();
-    //     System.out.println("Length: " + sol.length);
-    //     for (int i=0; i<sol.length/2; i++){
-    //         System.out.println( "X" + (i+1) + ": " + Math.round(sol[i]) + "\ty" + (i+1) + ": " + Math.round(sol[i+1]) );
-    //     }
-    // }
 }
